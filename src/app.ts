a9161d32fb122091f48968d4eaa31a3df40d74a8
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-10-26 22:28:28 -0300

    feat: Create organizations

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,61 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import fastify from 'fastify'
+import { z } from 'zod'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/organizations', async (request, reply) => {
+  const registerBodySchema = z.object({
+    responsible: z.string(),
+    email: z.string().email(),
+    cep: z.string(),
+    address: z.string(),
+    latitude: z.number(),
+    longitude: z.number(),
+    whatsapp: z.string(),
+    password: z.string().min(6),
+  })
+
+  const {
+    responsible,
+    email,
+    cep,
+    address,
+    latitude,
+    longitude,
+    whatsapp,
+    password,
+  } = registerBodySchema.parse(request.body)
+
+  const emailAlreadyExists = await prisma.organization.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (emailAlreadyExists) {
+    return reply.status(400).send({ message: 'Email already exists' })
+  }
+
+  const password_hash = await hash(password, 6)
+
+  try {
+    await prisma.organization.create({
+      data: {
+        responsible,
+        email,
+        cep,
+        address,
+        latitude,
+        longitude,
+        whatsapp,
+        password_hash,
+      },
+    })
+  } catch (err) {
+    return reply.status(400).send()
+  }
+
+  return reply.status(201).send()
+})
